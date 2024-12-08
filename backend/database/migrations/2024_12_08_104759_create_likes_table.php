fede6703d591541f5ba102a0150ea1dcc63fee72
Ananthakumar <anandmech46@gmail.com>
2024-12-08 16:29:12 +0530

    setting up project

---
+++ backend/database/migrations/2024_12_08_104759_create_likes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id'); // The post being liked
+            $table->unsignedBigInteger('user_id'); // User who liked the post
+            $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            // Prevent duplicate likes based on user
+            $table->unique(['post_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
